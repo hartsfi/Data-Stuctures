a8030ef2051ecef79c08a0d85253a7fb2f4f251b
dandoij <161157332+dandoij@users.noreply.github.com>
2025-02-03 23:04:45 -0500

    Update Game.cpp

--- Lab2/Game.cpp
+++ Lab2/Game.cpp
@@ -1,4 +1,12 @@
 #include "Game.h"
 using namespace std;
 
-Game::Game() : teamOneScore(0), teamTwoScore(0), possesion("Unknown") {}
+Game::Game() : teamOneScore(0), teamTwoScore(0), numPossessions(60), possession("Unknown") {}
+
+void Game::printScores() {
+
+	cout << "Current Scores: \n"
+		<< "Team One: " << teamOneScore << "points\n"
+		<< "Team Two: " << teamTwoScore << "points\n\n"
+		<< "Possesions Left: " << numPossessions << endl;
+}
