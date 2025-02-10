80ae451cf2cc937fb13c0d73bc3959013324d4d5
dandoij <161157332+dandoij@users.noreply.github.com>
2025-02-09 22:10:58 -0500

    Update Measurement.h

--- Lab3/Measurement.h
+++ Lab3/Measurement.h
@@ -7,24 +7,33 @@ using namespace std;
 class Measurement {
 
 private:
+
 	int little;
 	int lot;
 	int heap;
 
 public:
+
 	Measurement();
 	Measurement(int little, int lot, int heap);
 	Measurement(int little);
-	
-	int getLittle() {return little;}
-	int getLot() {return lot;}
-	int getHeap() {return heap;}
-	
-	void printMeasurement();
 
-	void setLittle(int littleNum) {little = littleNum;}
+	int getLittle() { return little; }
+	int getLot() { return lot; }
+	int getHeap() { return heap; }
+
+	void setLittle(int littleNum) { little = littleNum; }
 	void setLot(int lotNum) { lot = lotNum; }
-	void getHeap(int heapNum) {heap = heapNum;}
+	void setHeap(int heapNum) { heap = heapNum; }
 
+	void convertToLittles();
+	void convertFromLittles();
+	void simplifyMeasurement();
+
+	bool operator ==(Measurement measurement);
+	void operator +=(Measurement measurement);
+	void operator -=(Measurement measurement);
+	void operator <<(Measurement measurement);
+	friend ostream& operator<<(ostream& os, const Measurement& measurement);
 
 };
