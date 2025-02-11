be8fcfe52b7568e785a2d843f7c0d1d7443111c4
dandoij <161157332+dandoij@users.noreply.github.com>
2025-02-10 21:26:47 -0500

    Update Measurement.h

--- Lab3/Measurement.h
+++ Lab3/Measurement.h
@@ -8,35 +8,40 @@ class Measurement {
 
 private:
 
+	//Private members
 	int little;
 	int lot;
 	int heap;
 
 public:
 
+	//Constuctor declarations
 	Measurement();
 	Measurement(int little, int lot, int heap);
 	Measurement(int little);
 
+	//Getters / Accessors
 	int getLittle() { return little; }
 	int getLot() { return lot; }
 	int getHeap() { return heap; }
 
+	//Setters / Mutators
 	void setLittle(int littleNum) { little = littleNum; }
 	void setLot(int lotNum) { lot = lotNum; }
 	void setHeap(int heapNum) { heap = heapNum; }
 
+	//Member function declarations
+	int returnMeters();
 	void convertToLittles();
 	void convertFromLittles();
 	void simplifyMeasurement();
 
+	//Operator overload declarations
 	bool operator ==(Measurement measurement);
-	
 	void operator -(Measurement measurement);
 	void operator +(Measurement measurement);
 	void operator *(Measurement measurement);
 	void operator /(Measurement measurement);
-
 	friend ostream& operator<<(ostream& os, const Measurement& measurement);
 
 };
