06121dc3ceb16f9eea8fc847cc7ecb9ade7711b2
MoeAdj <157937026+MoeAdj@users.noreply.github.com>
2025-02-20 18:56:52 -0500

    Create wheel.cpp

---
+++ Homework/wheel.cpp
@@ -0,0 +1,21 @@
+#include "wheel.h"
+#include <cstdlib>
+#include <ctime>
+
+Wheel::Wheel(int maxVal) {
+    minValue = 1;
+    maxValue = maxVal;
+    srand(time(0));
+}
+
+int Wheel::spin() {
+    return rand() % (maxValue - minValue + 1) + minValue;
+}
+
+void Wheel::increaseRange() {
+    if (maxValue < 20) maxValue++;
+}
+
+void Wheel::decreaseRange(int playerMax) {
+    if (maxValue > playerMax) maxValue--;
+}
