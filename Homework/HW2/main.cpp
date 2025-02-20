6e13f5beb13a95d8eaaf562248f687943ce0d513
MoeAdj <157937026+MoeAdj@users.noreply.github.com>
2025-02-20 14:57:59 -0500

    Update main.cpp

--- Homework/HW2/main.cpp
+++ Homework/HW2/main.cpp
@@ -1,47 +1,80 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include "Player.h"
+#include "Wheel.h"
+
 using namespace std;
 
 int main() {
-        Player player;
-        int betAmount;
-        char playAgain;
-    
-        do {
-            // Check if player has money left
-            if (player.money <= 0) {
-                cout << "You're out of money! Game over." << endl;
-                break;
-            }
-    
-            // Ask for a bet
-            cout << "You have $" << player.money << ". Enter your bet: ";
-            cin >> betAmount;
-            player.placeBet(betAmount);
-    
-            // Spin the player's wheel
-            int playerResult = player.spinWheel();
-            cout << "Your wheel landed on: " << playerResult << endl;
-    
-            // For now, house spins once (we'll add more logic later)
-            Wheel houseWheel;
+    Player player;
+    Wheel houseWheel;
+    int betAmount;
+    char playAgain, wagerChoice, hardMode;
+    bool isHardMode = false;
+    int houseWinStreak = 0;
+
+    cout << "Enable hard mode? (y/n): ";
+    cin >> hardMode;
+    if (hardMode == 'y' || hardMode == 'Y') {
+        isHardMode = true;
+    }
+
+    do {
+        if (player.money <= 0) {
+            cout << "You're out of money! Game over." << endl;
+            break;
+        }
+
+        cout << "You have $" << player.money << ". Enter your bet: ";
+        cin >> betAmount;
+        player.placeBet(betAmount);
+
+        int playerResult = player.spinWheel();
+        cout << "Your wheel landed on: " << playerResult << endl;
+
+        wagerChoice = player.chooseWagerOption();
+        player.adjustBet(wagerChoice);
+
+        bool playerWins = false;
+
+        if (wagerChoice == 'D' || wagerChoice == 'd') {
+            int house1 = houseWheel.spin();
+            int house2 = houseWheel.spin();
+            cout << "House spins: " << house1 << ", " << house2 << endl;
+            playerWins = !(house1 >= playerResult || house2 >= playerResult);
+        } else if (wagerChoice == 'H' || wagerChoice == 'h') {
+            int house1 = houseWheel.spin();
+            int house2 = houseWheel.spin();
+            cout << "House spins: " << house1 << ", " << house2 << endl;
+            playerWins = !(house1 >= playerResult && house2 >= playerResult);
+        } else {
             int houseResult = houseWheel.spin();
             cout << "House wheel landed on: " << houseResult << endl;
-    
-            // Compare results
-            if (playerResult > houseResult) {
-                player.win();
+            playerWins = playerResult > houseResult;
+        }
+
+        if (playerWins) {
+            player.win();
+            houseWinStreak = 0;
+        } else {
+            player.lose();
+            houseWinStreak++;
+        }
+
+        if (isHardMode) {
+            if (houseWinStreak >= 2) {
+                houseWheel.increaseRange();
+                houseWinStreak = 0;
             } else {
-                player.lose();
+                houseWheel.decreaseRange(player.wheel.maxValue);
             }
-    
-            // Ask if the player wants to play again
-            cout << "Play again? (y/n): ";
-            cin >> playAgain;
-    
-        } while (playAgain == 'y' || playAgain == 'Y');
-    
-        cout << "Thanks for playing!" << endl;
-        return 0;
-    }    
+        }
+
+        cout << "Play again? (y/n): ";
+        cin >> playAgain;
+
+    } while (playAgain == 'y' || playAgain == 'Y');
+
+    cout << "Thanks for playing!" << endl;
+    return 0;
+}
+
