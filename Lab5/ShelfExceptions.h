470856371197d64f98295b24d9699ba2d882749a
dandoij <161157332+dandoij@users.noreply.github.com>
2025-02-24 18:12:07 -0500

    Create ShelfExceptions.h

---
+++ Lab5/ShelfExceptions.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+//Exception class for when the shelf is full
+class isFullException {
+    private:
+
+        string message;
+
+    public:
+
+        explicit isFullException(const string& msg) : message(msg) {}
+
+        string getMessage() const {
+            return message;
+        }
+};
+
+//Exception class for when the shelf is empty
+class isEmptyException {
+    private:
+
+        string message;
+
+    public:
+
+        explicit isEmptyException(const string msg) : message(msg) {}
+
+        string getMessage() const {
+            return message;
+        }
+};
+
+
