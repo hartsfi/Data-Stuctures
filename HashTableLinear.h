0b35f0941143ea5ded11fac5fbf194085b4c66b0
dandoij <161157332+dandoij@users.noreply.github.com>
2025-04-14 23:35:08 -0400

    Update HashTableLinear.h

--- HashTableLinear.h
+++ HashTableLinear.h
@@ -2,7 +2,7 @@
 #pragma once
 
 class HashTableLinear {
-    private:
+private:
     Student** table;//array of poiters to student 
     int maxSize;
     int currentSize;
@@ -10,11 +10,13 @@ class HashTableLinear {
     int hash(std::string key) const;
 
 public:
-HashTableLinear(int size = 100);
-~HashTableLinear(); // Destructor
+    HashTableLinear(int size = 100);
+    ~HashTableLinear(); // Destructor
 
-bool addItem(Student* s);
-Student* removeItem(std::string key); // remove item
-Student* getItem(std::string key);//get item without removing
-int getLength() const; // current number of items
+    bool addItem(Student* s);
+    Student* removeItem(std::string key); // remove item
+    Student* getItem(std::string key);//get item without removing
+    int getLength() const; // current number of items
+    bool isEmpty(); // returns true if table is empty
+    bool isInList(std::string key);
 };
