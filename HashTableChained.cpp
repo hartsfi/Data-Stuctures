c47e3c3579eee59688db5049851921477f06780b
Ethan <158618147+hartsfi@users.noreply.github.com>
2025-04-15 22:16:40 -0400

    Update HashTableChained.cpp

--- HashTableChained.cpp
+++ HashTableChained.cpp
@@ -37,7 +37,7 @@ Student* HashTableChained::removeItem(std::string inVal) {
     if (result != nullptr) {
         currentSize--;  // Decrement the current size of the hash table
         Student* s = new Student(result->data);  // Create a new student object with the data found
-        delete result;  // Delete the node from the linked list
+        chainedTable[index].RemoveItem(Student("", "", inVal, ""));
         return s;  // Return the student that was removed
     }
 
